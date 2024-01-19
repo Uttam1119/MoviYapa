3483fce31e42ee14274fc69d82c1134f106df024
Uttam <kumaruttam519@gmail.com>
2024-01-20 02:21:23 +0530

    corrected the import location

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo1 from "../../assets/originalLogo.png";
+import logo1 from "../../assets/OriginalLogo.png";
 
 const Header = () => {
   const [show, setShow] = useState("top");
